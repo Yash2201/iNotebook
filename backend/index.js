3b6c376cbe3885afc0879030cdf85102875a72e3
Yash Joshi <yashjoshi2201@gmail.com>
2022-12-24 10:08:14 +0530

    add note view and showing notes

--- backend/index.js
+++ backend/index.js
@@ -19,5 +19,5 @@ app.use('/api/note',require('./routes/note'));
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`iNotebook is listening on port ${port}`)
 })
